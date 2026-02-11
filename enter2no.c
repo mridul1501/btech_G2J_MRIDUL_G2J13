1e1654d80395260b85ebcbdf18bee9eff851d058
mridul1501 <jayantsharma1502@gmail.com>
2026-02-11 11:01:59 +0530

    Add files via upload

---
+++ enter2no.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+      printf("enter two number:\n");
+    scanf("%d%d",&a,&b);
+
+    if(a==b)
+    printf("two number are equal");
+    else 
+    printf("two number are not equal");
+    return 0;
+
+}
