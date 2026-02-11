34c5fa8346bd8be9d5b494bbdce68cb8e759874c
mridul1501 <jayantsharma1502@gmail.com>
2026-02-11 12:08:57 +0530

    Add files via upload

---
+++ evenno.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter number:\n");
+      scanf("%d",&a);
+
+    if(a%2==0)
+    {
+    printf("the given number is even");
+    }else 
+    {
+    
+        printf("the given number is odd");
+    }
+   
+    return 0;
+}
