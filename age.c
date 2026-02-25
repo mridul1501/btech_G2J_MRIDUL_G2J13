35d74f8a1c6242d96531645600c338c15c991bbf
mridul1501 <jayantsharma1502@gmail.com>
2026-02-25 11:53:23 +0530

    user age

---
+++ age.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+int a;
+printf("enter your age\n");
+scanf("%d",&a);
+if(a>=18)
+{
+    printf("user is eligible");
+}else{
+printf("user is not eligible");
+ }  
+ 
+ return 0;
+}
